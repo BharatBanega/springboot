2ab73f8ec997b83864ea04c63979d612dcb0d880
BharatBanega <126738109+BharatBanega@users.noreply.github.com>
2023-07-01 13:00:26 +0530

    Update ContentCollectionRepository.java

--- demo/src/main/java/com/example/demo/repository/ContentCollectionRepository.java
+++ demo/src/main/java/com/example/demo/repository/ContentCollectionRepository.java
@@ -1,1 +1,15 @@
+package com.example.demo.repository;
 
+import com.example.demo.content;
+import org.springframework.stereoType.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ContentCollectionRepository {
+
+  private final List<Content> content = new ArrayList<>();
+  
+
+}
